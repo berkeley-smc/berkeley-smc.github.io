8fc7cf6df5d4ffd0947721addded8374c36d200b
Michael Ball <michael@mball.co>
2023-08-29 03:16:38 -0700

    testing...

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./theme/theme"
 import NavBar from "./Components/Navbar/Navbar";
